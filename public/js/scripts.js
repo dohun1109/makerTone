a4422e1122f2d99cc019539f7946b27279e49462
dohun1109 <dohun1109@naver.com>
2023-10-05 18:39:46 +0900

    test v1.0.3

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -56,51 +56,6 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
-// 위치 정보 및 카카오 맵 초기화
-function initializeMap() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
-
-}
-
-// 위치 정보 획득 후 카카오 맵 실행
-function showPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    var container = document.getElementById('map_1');
-    var options = {
-        center: new kakao.maps.LatLng(latitude, longitude),
-        level: 2
-    };
-
-    var map = new kakao.maps.Map(container, options);
-
-    var markerPosition = new kakao.maps.LatLng(latitude, longitude);
-    var marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
-    marker.setMap(map);
-
-    var iwContent = '<div style="padding:5px;">현재 나의 위치 </div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
-
-// 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({
-        position : iwPosition,
-        content : iwContent
-    });
-
-// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-    infowindow.open(map, marker);
-
-    
-
-}
 
 
 
